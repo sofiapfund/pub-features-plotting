c2da7520b0b3ae1cfea1f6578aefc03d68eb29ef
Sofia <79449314+sofiapfund@users.noreply.github.com>
2021-04-12 10:20:21 +0200

    Update geoloc_animation.py
    - information retrieval through MongoDB
    - cleaner code

--- geoloc_animation.py
+++ geoloc_animation.py
@@ -8,53 +8,37 @@ Created on Tue Apr  6 21:18:12 2021
 
 # - Map of the geographic distribution of genome screening experiments using the first author affiliation location proxy. - #
 
-import requests
-import json
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import pandas as pd
 from matplotlib import animation
 import numpy as np
+from pymongo import MongoClient
 
+client = MongoClient() 
+cl = client['progenetix'].publications 
 
-def jprint(obj):
-    text = json.dumps(obj, sort_keys= True, indent = 4)
-    print(text)
-
-    # 1) Select results:
-    
-response = requests.get("https://progenetix.org/services/publications/?method=details")
-print(response.status_code)
-publications = response.json()["response"]["results"]
-
-c = []
-for pub in publications:       
+### 1) Get data:
+rows = []
+for pub in cl.find({"year": {"$regex": r"\d{4}"}}):
     coord = pub["provenance"]["geo_location"]["geometry"]["coordinates"]
     year = pub["year"]
     genomes = pub["counts"]["genomes"]
     if coord != [] and coord !=[0,0] and year != None and genomes != None:
-        c.append([coord, year, genomes])
+        rows.append([coord, int(year), int(genomes)])
 
-    # 2) Sort data in chronological order:
-
+### 2) Sort data in chronological order:
 def takeFirst(elem):
      return elem[1]
-c.sort(key=takeFirst)
+rows.sort(key=takeFirst)
 
-    # 3) Plot geolocation of tumor samples across the years:
-
-years = []
-genomess = []
-for coord_pair in c:
-    years.append(int(coord_pair[1]))
-    genomess.append(coord_pair[2])
-
-uniq = list(set(years)) # 28 unique years
-
-df = pd.DataFrame({"Years": years, "Genomes": genomess})
+### 3) Make dataframe with all the data:
+df = pd.DataFrame(rows, columns=["Coordinates", "Years", "Genomes"])
+uniq = list(set(df["Years"]))
 df["yr_genome_sum"] = df.groupby(["Years"])["Genomes"].transform(sum)
 uniq_genomes = list(set(df["yr_genome_sum"])) # total amount of samples for each unique year
 
+### 4) Plot geolocation of tumor samples across the years:
 fig, ax = plt.subplots(figsize=(14,8))
 x_vals = []
 y_vals = []
@@ -68,12 +52,18 @@ my_map.drawmapboundary(color= "black", linewidth=0.5, fill_color='#3cb9e0')
 scatter = ax.scatter(x_vals,y_vals, color="red", s=30, alpha=0.3, zorder=1.5)
 ax.set_title("Geographic distribution of genome screening experiments from 1993 to 2020.", fontweight="bold", y=1.05, fontsize=14)
 
+cities = [["New York", -73.935242, 40.730610], ["London", -0.118092, 51.509865], ["New Delhi", 77.216721, 28.644800], 
+          ["Hong Kong", 114.177216, 22.302711], ["Sydney", 151.209900, -33.865143], ["São Paulo", -46.625290, -23.533773],
+          ["Lagos", 3.406448, 6.465422]]
+for city in cities:
+    plt.plot(city[1], city[2], "wo", markersize=3)
+    ax.annotate(city[0], (city[1]+0.5, city[2]+1.0), fontweight="bold", color="white")
+
 samples = 0
 def update(t):
     global x_vals, y_vals, intensity, samples
     
-    # Get intermediate points
-    for coord_pair in c:
+    for coord_pair in rows:
         if int(coord_pair[1]) == uniq[t]:
             new_xvals, new_yvals = coord_pair[0][0], coord_pair[0][1] # lons, lats
             x_vals.append(new_xvals)
@@ -85,8 +75,7 @@ def update(t):
     # Set title and add sample information (how many)
     samples += uniq_genomes[t]
     plt.xlabel("Year: " + str(uniq[t]) + "\n" + f"Samples: {samples}", fontsize=12, fontweight="bold", labelpad=15)
-
-        
+       
 ani = animation.FuncAnimation(fig, update, frames=len(uniq), interval=1500, repeat=False)
 
 plt.show()
